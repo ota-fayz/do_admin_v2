f14eb24d68b8fe9d97ba1d38d009eb40116b5e0b
ota-fayz <otabek@Otabeks-MacBook-Pro.local>
2022-02-27 14:23:30 +0500

    login logout

--- src/http/dataProvider.ts
+++ src/http/dataProvider.ts
@@ -1,1 +1,11 @@
-export {}
+export const apiUrl = "https://dev.polito.uz/api/token-auth/"
+
+// const httpClient = (url, options = {}) => {
+//     if (!options.headers) {
+//         options.headers = new Headers({ Accept: 'application/json' });
+//     }
+//     const { token } = JSON.parse(localStorage.getItem('auth'));
+//     options.headers.set('Authorization', `Bearer ${token}`);
+//     return fetchUtils.fetchJson(url, options);
+// };
+// const dataProvider = simpleRestProvider('https://dev.polito.uz/api/token-auth', httpClient);
